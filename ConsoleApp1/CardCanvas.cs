7f8656c2f8995dad962058446bf49978c6e24db2
Takuya Omori <takuya.omori@sansan.com>
2021-04-26 15:23:39 +0900

    Init Commit

---
+++ ConsoleApp1/CardCanvas.cs
@@ -0,0 +1,36 @@
+﻿using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+
+namespace ConsoleApp1
+{
+	internal class CardCanvas
+	{
+		public void CreateAndDraw(string filePath)
+		{
+			var txt = "　,　";
+			var fontFamily = SystemFonts.Find("Yu Gothic");
+			var font = new Font(fontFamily, 16);
+
+			using (var image = new Image<Rgba32>(600, 300, Color.White))
+			{
+				try
+				{
+					image.Mutate(x => x
+						.DrawText(txt, font, Color.Black, new PointF(0, 0))
+					);
+					image.SaveAsJpeg(filePath);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e);
+					throw;
+				}
+			}
+		}
+
+	}
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var cardCanvas = new CardCanvas();
+
+			//// case 1.
+			//cardCanvas.CreateAndDraw($"case1_{1:D3}.jpg");
+
+			// case 2.
+			var options = new ParallelOptions
+			{
+				MaxDegreeOfParallelism = 4
+			};
+			Parallel.ForEach(Enumerable.Range(1, 10), options, item =>
+			{
+				cardCanvas.CreateAndDraw($"case2_{item:D3}.jpg");
+			});
+		}
+	}
+}
